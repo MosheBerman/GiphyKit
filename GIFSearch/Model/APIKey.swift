62d1f397ed46e885b7e6d5a4c85810799c889b11
Moshe Berman <mosheberman@users.noreply.github.com>
2017-08-27 11:57:23 -0400

    Added a file to store the GIPHY API key.

---
+++ GIFSearch/Model/APIKey.swift
@@ -0,0 +1,12 @@
+//
+//  APIKey.swift
+//  GIFSearch
+//
+//  Created by Moshe Berman on 8/27/17.
+//  Copyright © 2017 Moshe Berman. All rights reserved.
+//
+
+import Foundation
+
+/// Put your GIPHY API Key here
+let APIKey: String = ""
