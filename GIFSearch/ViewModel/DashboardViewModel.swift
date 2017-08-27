ed3af19c6479ebd0f20cbd286de33c78a8f76e1d
Moshe Berman <mosheberman@users.noreply.github.com>
2017-08-27 11:59:10 -0400

    Added a viewModel class for the dashboard.

---
+++ GIFSearch/ViewModel/DashboardViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  DashboardViewModel.swift
+//  GIFSearch
+//
+//  Created by Moshe Berman on 8/27/17.
+//  Copyright © 2017 Moshe Berman. All rights reserved.
+//
+
+import Foundation
+
+class DashboardViewModel: NSObject {
+
+}
