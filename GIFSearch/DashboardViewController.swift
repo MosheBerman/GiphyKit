34a10cba4486e4f73c1a4c1834e5e26c6a05b853
Moshe Berman <mosheberman@users.noreply.github.com>
2017-08-28 11:33:07 -0400

    Renamed ViewController to DashboardViewController.

---
+++ GIFSearch/DashboardViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DashboardViewController.swift
+//  GIFSearch
+//
+//  Created by Moshe Berman on 8/27/17.
+//  Copyright © 2017 Moshe Berman. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
