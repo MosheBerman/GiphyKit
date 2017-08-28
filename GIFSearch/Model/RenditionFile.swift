54582f027de3d3b61276612305c4d4852610031e
Moshe Berman <mosheberman@users.noreply.github.com>
2017-08-28 19:39:42 -0400

    Implemented the RenditionFile class.

--- GIFSearch/Model/RenditionFile.swift
+++ GIFSearch/Model/RenditionFile.swift
@@ -10,10 +10,32 @@ import Foundation
 
 class RenditionFile: NSObject {
 
-    
     /// The format of the file.
     let fileType: RenditionFileType
     
+    /// The URL for the file.
+    let url: URL
     
+    /// How large the file is, in bytes.
+    let sizeInBytes: NSInteger
+    
+    
+    init?(of type: RenditionFileType, with json: [String: Any])
+    {
+        guard let sizeString = json[type.sizeKey] as? String,
+        let sizeNumber = numberFormatter.number(from: sizeString),
+            let address = json[type.rawValue] as? String,
+            let url = URL(string: address)
+        else
+        {
+            return nil
+        }
+        
+        self.fileType = type
+        self.sizeInBytes = sizeNumber.intValue
+        self.url = url
+        
+        super.init()
+    }
     
 }
