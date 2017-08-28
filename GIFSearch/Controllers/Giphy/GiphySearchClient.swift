07628b12aadf2555dde773173f48e1edbfa8de59
Moshe Berman <mosheberman@users.noreply.github.com>
2017-08-28 17:03:26 -0400

    Start implement the Giphy Search Client.

--- GIFSearch/Controllers/Giphy/GiphySearchClient.swift
+++ GIFSearch/Controllers/Giphy/GiphySearchClient.swift
@@ -10,4 +10,21 @@ import Foundation
 
 class GiphySearchClient: NSObject {
     
+    /// The GIPHY root endpoint.
+    private var rootEndpoint: String = "https://api.giphy.com/v1/gifs/search"
+    
+    /// Replace this with your API key.
+    private let apiKey: String = APIKey
+    
+    // MARK: - Getting Trending GIFs.
+    
+    /// Fetch the trending GIFs
+    ///
+    /// - Parameters:
+    ///   - rating: The maximum filter rating to use with the request.
+    ///   - completion: A completion handler to execute after downloading and processing the data.
+    public func trending(for rating: Rating = .g, with completion:())
+    {
+        
+    }
 }
