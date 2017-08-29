a3a2e2883c3676a5700451527607cbf0f1f50d24
Moshe Berman <mosheberman@users.noreply.github.com>
2017-08-29 16:34:45 -0400

    Continue implementing API client.

--- GIFSearch/Controllers/Giphy/GiphySearchClient.swift
+++ GIFSearch/Controllers/Giphy/GiphySearchClient.swift
@@ -10,12 +10,42 @@ import Foundation
 
 class GiphySearchClient: NSObject {
     
-    /// The GIPHY root endpoint.
-    private var rootEndpoint: String = "https://api.giphy.com/v1/gifs/search"
+    // MARK: - The Client API Key
     
     /// Replace this with your API key.
     private let apiKey: String = APIKey
     
+    // MARK: - Endpoints
+    
+    /// The GIPHY root endpoint.
+    private var rootEndpoint: URLComponents
+    
+    /// The endpoint for trending
+    private var trendingEndpoint: URL? {
+        var components = self.rootEndpoint
+        components.path = "trending"
+        components.queryItems = [
+            URLQueryItem(name: "api_key", value: self.apiKey)
+        ]
+        
+        guard let url = components.url else
+        {
+            return nil
+        }
+        
+        return url
+    }
+    
+    
+    // MARK: -
+    
+    override init() {
+        
+        self.rootEndpoint = URLComponents(string: "https://api.giphy.com/v1")!
+        
+        super.init()
+    }
+
     // MARK: - Getting Trending GIFs.
     
     /// Fetch the trending GIFs
@@ -23,8 +53,60 @@ class GiphySearchClient: NSObject {
     /// - Parameters:
     ///   - rating: The maximum filter rating to use with the request.
     ///   - completion: A completion handler to execute after downloading and processing the data.
-    public func trending(for rating: Rating = .g, with completion:())
+    public func trending(for rating: Rating = .g, with completion:(@escaping ([GIF]?, NSError?)->Void))
     {
+        guard let endpoint = self.trendingEndpoint else
+        {
+            var error = NSError(domain: "com.gifyclient.endpoint", code: 1, userInfo: nil)
+            completion(nil, error)
+            return
+        }
         
+        let task = URLSession.shared.dataTask(with: endpoint) { (data:Data?, response:URLResponse?, error:Error?) in
+            
+            var errorResponse: NSError? = nil
+            var gifs: [GIF]? = nil
+            
+            if let data = data, error == nil
+            {
+                do
+                {
+                    
+                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any],
+                    let jsonOfGIFs = json["data"] as? [[String:String]]
+                    {
+                        for json in jsonOfGIFs
+                        {
+                            if let gif = GIF(with: json)
+                            {
+                                gifs?.append(gif)
+                            }
+                            else
+                            {
+                                print("\(self.self) : Failed to process GIF for JSON: \(json)")
+                            }
+                        }
+                    }
+                    else
+                    {
+                       errorResponse = NSError(domain: "com.gifyclient.url-session-failure", code: ErrorCode.failedToUnwrapJSONFromDataResponse.rawValue, userInfo: nil)
+                    }
+                    
+                }
+                catch
+                {
+                    errorResponse = NSError(domain: "com.gifyclient.url-session-failure", code: ErrorCode.failedToUnwrapJSONFromDataResponse.rawValue, userInfo: nil)
+                }
+            }
+            else
+            {
+                errorResponse = NSError(domain: "com.gifyclient.url-session-failure", code: ErrorCode.noDataInResponse.rawValue, userInfo: nil)
+            }
+            
+            completion(gifs, errorResponse)
+        }
+        
+        
+        task.resume()
     }
 }
