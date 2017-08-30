a821b7e7470d77a9d8b6ce9c80569bef6fea9dfa
Moshe Berman <mosheberman@users.noreply.github.com>
2017-08-30 13:25:56 -0400

    Added some rendition designations.

--- GiphyKit/Model/RenditionDesignation.swift
+++ GiphyKit/Model/RenditionDesignation.swift
@@ -22,12 +22,15 @@ import Foundation
 /// - fixedWidthSmall - Width set to 100px. Good for mobile keyboards
 /// - fixedWidthSmallStill - Static preview image for fixed_width_small
 /// - preview - File size under 50kb. Duration may be truncated to meet file size requirements. Good for thumbnails and previews.
+/// - previewGIF - The preview as a GIF.
+/// - previewWebP - The preview in WebP format.
 /// - downsizedSmall - File size under 200kb.
 /// - downsized - File size under 2mb.
 /// - downsizedMedium - File size under 5mb.
 /// - downsizedLarge - File size under 8mb.
 /// - downsizedStill - Static preview image for downsized
 /// - original - Original file size and file dimensions. Good for desktop use.
+/// - originalMP4 - The original, encoded as mp4.
 /// - originalStill - Preview image for original
 /// - looping - Duration set to loop for 15 seconds. Only recommended for this exact use case.
 public enum RenditionDesignation: String {
@@ -42,12 +45,15 @@ public enum RenditionDesignation: String {
     case fixedWidthSmall = "fixed_width_small"
     case fixedWidthSmallStill = "fixed_width_small_still"
     case preview = "preview"
+    case previewGIF = "preview_gif"
+    case previewWebP = "preview_webp"
     case downsizedSmall = "downsized_small"
     case downsizedMedium = "downsized_medium"
     case downsizedLarge = "downsized_large"
     case downsizedStill = "downsized_still"
     case original = "original"
     case originalStill = "original_still"
+    case originalMP4 = "original_mp4"
     case looping = "looping"
     
 }
