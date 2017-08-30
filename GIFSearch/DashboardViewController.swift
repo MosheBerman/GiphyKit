74ac5052abd6f45854c72127fd5510bb988c49ff
Moshe Berman <mosheberman@users.noreply.github.com>
2017-08-30 13:12:24 -0400

    Implement collection view boilerplate, and refresh code. Also add pull-to-refresh.

--- GIFSearch/DashboardViewController.swift
+++ GIFSearch/DashboardViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class DashboardViewController: UIViewController {
+class DashboardViewController: UIViewController, UICollectionViewDataSource {
 
     @IBOutlet weak var collectionView: UICollectionView!
     private let viewModel = DashboardViewModel()
@@ -16,6 +16,9 @@ class DashboardViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.configureViewModel()
+        self.configureCollectionView()
+        self.viewModel.setNeedsRefresh()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +26,59 @@ class DashboardViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - Configuring the Collection View
+    func configureCollectionView()
+    {
+        self.collectionView.dataSource = self
+        
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        self.collectionView.refreshControl = refreshControl
+        
+    }
+    
+    // MARK: - Configuring Our Response to ViewModel Updates
+    
+    /// Sets a refresh handler that is executed whenever the 
+    /// ViewModel refreshes its data.
+    func configureViewModel()
+    {
+        self.viewModel.refreshHandler = { [weak self] in
+            
+            guard let strongSelf = self else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                strongSelf.collectionView.reloadData()
+                strongSelf.collectionView.refreshControl?.endRefreshing()
+            }
+        }
 
+    }
+    
+    // MARK: - Manually Refreshing
+    
+    func refresh()
+    {
+        self.collectionView.refreshControl?.beginRefreshing()
+        self.viewModel.setNeedsRefresh()
+    }
+    
+    // MARK: - UICollectionViewDataSource
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.viewModel.numberOfItems(in: section)
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return self.viewModel.numberOfSections
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "com.mosheberman.cell", for: indexPath)
+        
+        return cell
+    }
 }
 
