8722b5f297095f505a669e8ed7adf5318cd9f1ab
Moshe Berman <mosheberman@users.noreply.github.com>
2017-08-30 13:26:28 -0400

    Added the ability to the view model to return a GIF object corresponding to an index path.

--- GIFSearch/ViewModel/DashboardViewModel+CollectionView.swift
+++ GIFSearch/ViewModel/DashboardViewModel+CollectionView.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import GiphyKit
 
 extension DashboardViewModel
 {
@@ -30,6 +31,30 @@ extension DashboardViewModel
         }
         
         return count
+    }
+    
+    // MARK: - Accessing GIF Data
+    
+    
+    /// Looks up the gif corresponding to a given index path. If one exists, returns it.
+    ///
+    /// - Parameter indexPath: The indexpath to display the GIF at.
+    /// - Returns: A GIF, if one exists, otherwise `nil`.
+    func gif(for indexPath: IndexPath) -> GIF?
+    {
+        var gif: GIF? = nil
+        
+        guard let gifs = self.gifs else
+        {
+            return gif
+        }
+        
+        if indexPath.row < gifs.count
+        {
+            gif = gifs[indexPath.row]
+        }
+        
+        return gif
     }
     
 }
