d5a27d7b5799f3ff8d3b15ce8a8fe2257c3eaf9f
Moshe Berman <mosheberman@users.noreply.github.com>
2017-08-30 13:11:49 -0400

    Rename custom cell class.

---
+++ GIFSearch/View/Custom Cells/GIFCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  GIFCollectionViewCell.swift
+//  GIFSearch
+//
+//  Created by Moshe Berman on 8/30/17.
+//  Copyright © 2017 Moshe Berman. All rights reserved.
+//
+
+import UIKit
+
+class GIFCollectionViewCell: UICollectionViewCell {
+    
+}
