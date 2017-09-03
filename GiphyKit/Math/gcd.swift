3dcc2c4e4f49e65d3f132589d2270d78af4a0ca6
Moshe Berman <mosheberman@users.noreply.github.com>
2017-09-03 13:19:59 -0400

    Implement a GCD implementation, based on Euclids algorithm of continuously dividing the remainder of a / b until the modulo is zero.

---
+++ GiphyKit/Math/gcd.swift
@@ -0,0 +1,32 @@
+//
+//  gcd.swift
+//  GIFSearch
+//
+//  Created by Moshe Berman on 9/3/17.
+//  Copyright © 2017 Moshe Berman. All rights reserved.
+//
+
+import Foundation
+
+/// Use Euclid's algorithm, describe here: https://en.wikipedia.org/wiki/Greatest_common_divisor#Other_methods
+///
+/// - Parameters:
+///   - a: The larger number.
+///   - b: The smaller number.
+/// - Returns: The greatest common divisor.
+public func gcd(a: Int, b: Int) -> Int
+{
+    if b > a
+    {
+        return gcd(a: b, b: a)
+    }
+    
+    if b == 0
+    {
+        return a
+    }
+    
+    let remainder = a % b
+    
+    return gcd(a: b, b: remainder)
+}
