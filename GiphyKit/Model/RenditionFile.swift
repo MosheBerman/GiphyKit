c6a0704c2162ff30516ddcd879d98be2dfc2a881
Moshe Berman <mosheberman@users.noreply.github.com>
2017-09-03 14:17:24 -0400

    Process rendition height/width info as part of initializing from JSON.

--- GiphyKit/Model/RenditionFile.swift
+++ GiphyKit/Model/RenditionFile.swift
@@ -20,6 +20,13 @@ public class RenditionFile: NSObject {
     /// How large the file is, in bytes.
     public let sizeInBytes: NSInteger
     
+    /// The height of the image.
+    public let height: Int?
+    
+    
+    /// The width of the image.
+    public let width: Int?
+    
     // MARK: - Initializing a Rendition
     
     public init?(of type: RenditionFileType, with json: [String: Any])
@@ -36,6 +43,24 @@ public class RenditionFile: NSObject {
         self.fileType = type
         self.sizeInBytes = sizeNumber.intValue
         self.url = url
+        
+        if let height = json["height"] as? String
+        {
+            self.height = numberFormatter.number(from: height)?.intValue
+        }
+        else
+        {
+            self.height = nil
+        }
+        
+        if let width = json["width"] as? String
+        {
+            self.width = numberFormatter.number(from: width)?.intValue
+        }
+        else
+        {
+            self.width = nil
+        }
         
         super.init()
     }
