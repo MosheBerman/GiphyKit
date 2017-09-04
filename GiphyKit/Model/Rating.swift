7c42f955297996b045e6f91f479f43c1b56455ba
Moshe Berman <mosheberman@users.noreply.github.com>
2017-09-04 06:15:52 -0400

    Added a public var, `displayName`, to the Rating enum.

--- GiphyKit/Model/Rating.swift
+++ GiphyKit/Model/Rating.swift
@@ -21,4 +21,19 @@ public enum Rating: String {
     case pg13 = "PG-13"
     case r = "R"
     case unrated = "?"
+    
+    public var displayName: String {
+        switch self {
+        case .g:
+            return "G"
+        case .pg13:
+            return "PG-13"
+        case .pg:
+            return "PG"
+        case .r:
+            return "R"
+        case .unrated:
+            return "Unrated"
+        }
+    }
 }
