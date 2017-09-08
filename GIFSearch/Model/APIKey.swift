c08f31feaeb8084d30901cc359a05e295b4f6b64
Moshe Berman <moshe.berman@gamechanger.com>
2017-09-08 12:46:42 -0400

    Added instructions for setting up the API key.

--- GIFSearch/Model/APIKey.swift
+++ GIFSearch/Model/APIKey.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+/**
+ Get an API key from https://developers.giphy.com/dashboard/
+ 
+ Before saving it here, you might want to tell git to 
+ ignore future changes to this file, so you don't commit
+ your API key back to your repository.
+ 
+ Do that with this command:
+ 
+ > cd /path/to/GiphyKit/
+ > git update-index --assume-unchanged ./GIFSearch/Model/APIKey.swift
+ 
+ Read more about how this works here: https://stackoverflow.com/a/18277622/224988
+ 
+ You should be all set!
+ 
+ */
+let APIKey = "<#Giphy API Key#>"
+
